5d5aca8a4e5e50c6c557c773fa1e2dcbe56a1b95
psontay <phamsontay2206@gmail.com>
2026-06-13 15:40:20 +0700

    feat: application.yml

---
+++ shopflow-identity/src/main/java/com/shopflow/identity/ShopflowIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.shopflow.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopflowIdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopflowIdentityApplication.class, args);
+    }
+
+}
