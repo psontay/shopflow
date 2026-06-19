e3594a9ea1bcd6f0505b5db3e689c472be926fe7
Sontaypham <phamsontay2206@gmail.com>
2026-06-19 15:27:01 +0700

    feat: refresh token repo impl

--- shopflow-identity/src/main/java/com/shopflow/identity/infrastructure/persistence/JpaRefreshTokenRepository.java
+++ shopflow-identity/src/main/java/com/shopflow/identity/infrastructure/persistence/JpaRefreshTokenRepository.java
@@ -1,12 +1,18 @@
 package com.shopflow.identity.infrastructure.persistence;
 
 import com.shopflow.identity.infrastructure.persistence.entity.RefreshTokenEntity;
+import com.shopflow.identity.infrastructure.persistence.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface JpaRefreshTokenRepository extends JpaRepository<RefreshTokenEntity, UUID> {
+
+    Optional<RefreshTokenEntity> findByToken(String token);
+
+    void deleteByUser(UserEntity userEntity);
 
 }
--- shopflow-identity/src/main/java/com/shopflow/identity/infrastructure/persistence/impl/RefreshTokenRepositoryImpl.java
+++ shopflow-identity/src/main/java/com/shopflow/identity/infrastructure/persistence/impl/RefreshTokenRepositoryImpl.java
@@ -4,25 +4,36 @@ import com.shopflow.identity.domain.models.RefreshToken;
 import com.shopflow.identity.domain.models.User;
 import com.shopflow.identity.domain.repository.RefreshTokenRepository;
 import com.shopflow.identity.infrastructure.persistence.JpaRefreshTokenRepository;
+import com.shopflow.identity.infrastructure.persistence.entity.UserEntity;
+import com.shopflow.identity.infrastructure.persistence.mapper.RefreshTokenPersistenceMapper;
+import com.shopflow.identity.infrastructure.persistence.mapper.UserPersistenceMapper;
 
 import java.util.Optional;
 
 public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
 
     private final JpaRefreshTokenRepository jpaRefreshTokenRepository;
+    private final RefreshTokenPersistenceMapper refreshTokenPersistenceMapper;
+    private final UserPersistenceMapper userPersistenceMapper;
 
-    public RefreshTokenRepositoryImpl(JpaRefreshTokenRepository jpaRefreshTokenRepository) {
+    public RefreshTokenRepositoryImpl(JpaRefreshTokenRepository jpaRefreshTokenRepository,
+                                      RefreshTokenPersistenceMapper refreshTokenPersistenceMapper,
+                                      UserPersistenceMapper userPersistenceMapper) {
         this.jpaRefreshTokenRepository = jpaRefreshTokenRepository;
+        this.refreshTokenPersistenceMapper = refreshTokenPersistenceMapper;
+        this.userPersistenceMapper = userPersistenceMapper;
     }
 
     @Override
     public Optional<RefreshToken> findByToken(String token) {
-        return Optional.empty();
+        return jpaRefreshTokenRepository.findByToken(token)
+                                        .map(refreshTokenPersistenceMapper :: toDomain);
     }
 
     @Override
     public void deleteByUser(User user) {
-
+        UserEntity userEntity = userPersistenceMapper.toEntity(user);
+        jpaRefreshTokenRepository.deleteByUser(userEntity);
     }
 
 }
