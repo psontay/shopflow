64d5de5bb61fd2aba799270ec23b6d338eb6e658
Sontaypham <phamsontay2206@gmail.com>
2026-07-04 20:43:03 +0700

    feat: impl gateway with port 8888

---
+++ shopflow-gateway/src/main/java/com/shopflow/gateway/ShopflowGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shopflow.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopflowGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopflowGatewayApplication.class, args);
+    }
+
+}
