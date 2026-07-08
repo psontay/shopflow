72d57f20e2f987ad93921f95a0c6a08162861b13
Sontaypham <phamsontay2206@gmail.com>
2026-07-08 10:48:50 +0700

    feat: payment entity

---
+++ shopflow-payment/src/main/java/com/shopflow/payment/infrastructure/persistence/entity/PaymentEntity.java
@@ -0,0 +1,44 @@
+package com.shopflow.payment.infrastructure.persistence.entity;
+
+import com.shopflow.payment.domain.models.PaymentMethod;
+import com.shopflow.payment.domain.models.PaymentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentEntity {
+
+    @Id
+    private UUID id;
+    @Column(nullable = false)
+    private UUID orderId;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+    @Column(nullable = false)
+    private BigDecimal amount;
+    @Column(nullable = false)
+    private String providerTransactionId;
+
+}
