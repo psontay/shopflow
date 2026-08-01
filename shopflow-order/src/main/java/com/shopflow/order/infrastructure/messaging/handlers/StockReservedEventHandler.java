4514ecbcb5c47dc44f2e13fb53598830933dae5b
Sontaypham <phamsontay2206@gmail.com>
2026-08-01 16:32:04 +0700

    feat: handle mark as awaiting payment for reserved order

--- shopflow-order/src/main/java/com/shopflow/order/infrastructure/messaging/handlers/StockReservedEventHandler.java
+++ shopflow-order/src/main/java/com/shopflow/order/infrastructure/messaging/handlers/StockReservedEventHandler.java
@@ -2,6 +2,9 @@ package com.shopflow.order.infrastructure.messaging.handlers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shopflow.order.domain.exceptions.OrderDomainException;
+import com.shopflow.order.domain.exceptions.OrderErrorCode;
+import com.shopflow.order.domain.models.Order;
 import com.shopflow.order.domain.repositories.OrderRepository;
 import com.shopflow.order.infrastructure.persistence.repository.JpaProcessedEventRepository;
 import org.slf4j.Logger;
@@ -36,7 +39,11 @@ public class StockReservedEventHandler extends AbstractIdempotentEventHandler {
         String orderIdStr = rootNode.path("aggregateId")
                                     .asText();
         UUID orderId = UUID.fromString(orderIdStr);
-
+        Order order = orderRepository.findById(orderId)
+                                     .orElseThrow(() -> new OrderDomainException(OrderErrorCode.ORDER_NOT_FOUND));
+        order.markAsAwaitingPayment();
+        orderRepository.save(order);
+        log.info("Update order {} to PENDING_PAYMENT success", orderId);
     }
 
 }
