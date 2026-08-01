8fb6d5c71d40b2a36ab1d4c91056549a5b67012a
Sontaypham <phamsontay2206@gmail.com>
2026-08-01 16:17:11 +0700

    feat: handler for stock reserved

---
+++ shopflow-order/src/main/java/com/shopflow/order/infrastructure/messaging/handlers/StockReservedEventHandler.java
@@ -0,0 +1,42 @@
+package com.shopflow.order.infrastructure.messaging.handlers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shopflow.order.domain.repositories.OrderRepository;
+import com.shopflow.order.infrastructure.persistence.repository.JpaProcessedEventRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class StockReservedEventHandler extends AbstractIdempotentEventHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(StockReservedEventHandler.class);
+    private final ObjectMapper objectMapper;
+    private final OrderRepository orderRepository;
+
+    public StockReservedEventHandler(
+            JpaProcessedEventRepository jpaProcessedEventRepository,
+            OrderRepository orderRepository, ObjectMapper objectMapper) {
+        super(jpaProcessedEventRepository);
+        this.orderRepository = orderRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public boolean canHandle(String eventType) {
+        return "StockReservedEvent".equals(eventType);
+    }
+
+    @Override
+    protected void processBusinessLogic(String payload) throws Exception {
+        JsonNode rootNode = objectMapper.readTree(payload);
+        String orderIdStr = rootNode.path("aggregateId")
+                                    .asText();
+        UUID orderId = UUID.fromString(orderIdStr);
+
+    }
+
+}
